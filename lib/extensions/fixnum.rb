5ad9746e0cfd4d435b4956b6c9bc833b32a20a69
jspradlin <jspradlin@gmail.com>
2009-09-29 23:22:42 -0400

    Added methods to Fixnum to parse out portfolio,position, and transaction ids

---
+++ lib/extensions/fixnum.rb
@@ -0,0 +1,13 @@
+class Fixnum
+  def portfolio_id
+    self.to_s.portfolio_id
+  end
+  
+  def position_id
+    self.to_s.position_id
+  end
+  
+  def transaction_id
+    self.to_s.transaction_id
+  end
+end
